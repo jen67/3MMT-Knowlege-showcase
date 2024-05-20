3215b3400d9f015e636a6cbce330390cc3a7a97c
jen67 <amakrigift2000@gmail.com>
2024-05-20 19:26:25 +0100

    added some codes on the benefits section

---
+++ Frontend/src/Pages/Home/Benefits/Talents/Talents.js
@@ -0,0 +1,56 @@
+import images from "../../../../Components/images";
+import "./Talents.css";
+
+const Talents = () => {
+    return (
+      <section>
+        <div className="benefits-talents">
+          <div className="container">
+            <h2>Benefits for Talents</h2>
+            <div className="Talents">
+              <img
+                src={images.talentImg}
+                alt="Illustration representing talents"
+              />
+            </div>
+            <div className="company-flex">
+              <div className="row">
+                <div className="col-1-2 side-content">
+                  <p>Talents on Wefind enjoy a host of benefits:</p>
+                  <ul>
+                    <li>
+                      Access to a wide range of job opportunities across various
+                      industries and skill sets.
+                    </li>
+                    <li>
+                      Personalized recommendations based on your skills and
+                      preferences.
+                    </li>
+                    <li>
+                      Networking opportunities with industry professionals.
+                    </li>
+                    <li>
+                      Career development resources and support to help you
+                      thrive in your career.
+                    </li>
+                  </ul>
+                  <p>
+                    Unlock endless possibilities and take control of your career
+                    journey with Wefind!
+                  </p>
+                  <button
+                    className="button white-button"
+                    aria-label="Get started with Wefind"
+                  >
+                    Get started
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+};
+
+export default Talents;
--- Frontend/src/Pages/Home/Home.js
+++ Frontend/src/Pages/Home/Home.js
@@ -5,6 +5,7 @@ import images from "../../Components/images";
 import Subscribe from "./Subscribe/Subscribe";
 import Faq from "./Faq/Faq";
 import Testimonial from "./Testimonial/Testimonial";
+import Talents from "./Benefits/Talents/Talents";
 
 const Home = () => {
   return (
@@ -62,47 +63,7 @@ const Home = () => {
         </div>
       </section>
 
-      {/* <!-- Benefits for Talents Section --> */}
-      <section>
-        <div className="benefits-talents">
-          <div className="container">
-            <h2>Benefits for Talents</h2>
-            <div className="company-flex">
-              <div className="row">
-                <div className="col-1-2 side-content">
-                  <p>Talents on Wefind enjoy a host of benefits:</p>
-                  <ul>
-                    <li>
-                      Access to a wide range of job opportunities across various
-                      industries and skill sets.
-                    </li>
-                    <li>
-                      Personalized recommendations based on your skills and
-                      preferences.
-                    </li>
-                    <li>
-                      Networking opportunities with industry professionals.
-                    </li>
-                    <li>
-                      Career development resources and support to help you
-                      thrive in your career.
-                    </li>
-                  </ul>
-                  <p>
-                    Unlock endless possibilities and take control of your career
-                    journey with Wefind!
-                  </p>
-                  <button className="button">Get started</button>
-                </div>
-
-                <div className="col-1-2">
-                  <img src={images.talentImg} alt=" Talents" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+     <Talents />
       <Testimonial />
       <Faq />
       <Subscribe />
