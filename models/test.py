4258402318304800932748d785214cde30e60bbb
Omar <omarkhaled1752003@yahoo.com>
2024-05-21 15:56:50 +0300

    test file

---
+++ models/test.py
@@ -0,0 +1,24 @@
+import data.mongo_setup as mongo_setup
+import services.data_service as svc
+
+
+def main():
+    mongo_setup.global_init()
+
+    print(' ****************** REGISTER **************** ')
+
+    name = input('Enter your name: ')
+    email = input('Enter your email: ').strip().lower()
+    skills = ["flask", "python", "mongo"]
+
+    old_account = svc.find_user_my_email(email)
+    if old_account:
+        print(f"Error: Account with email {email} already exists.")
+        return
+
+    account = svc.create_account(name, email, skills)
+    print(f"Created new account with id {account.id}.")
+
+
+if __name__ == "__main__":
+    main()
