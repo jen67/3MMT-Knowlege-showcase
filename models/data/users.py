53f7519907150790cd031e7b1d66bb627a9525d0
Omar <omarkhaled1752003@yahoo.com>
2024-05-21 15:55:52 +0300

    users model

---
+++ models/data/users.py
@@ -0,0 +1,15 @@
+import datetime
+import mongoengine
+
+
+class User(mongoengine.Document):
+    registered_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+    name = mongoengine.StringField(required=True)
+    email = mongoengine.StringField(required=True)
+
+    skills = mongoengine.ListField()
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'users'
+    }
