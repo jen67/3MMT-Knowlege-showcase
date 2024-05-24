ce339ffd8089db1413fa7b31349401ca55e79a36
Omar <omarkhaled1752003@yahoo.com>
2024-05-24 14:45:39 +0300

    added more fields to the users model

--- models/data/users.py
+++ models/data/users.py
@@ -4,10 +4,14 @@ import mongoengine
 
 class User(mongoengine.Document):
     registered_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+
     name = mongoengine.StringField(required=True)
     email = mongoengine.StringField(required=True)
+    mobile = mongoengine.StringField(required=True)
+    password = mongoengine.StringField(required=True)
+    category = mongoengine.StringField(required=True)
 
-    skills = mongoengine.ListField(required=True)
+    # skills = mongoengine.ListField(required=True)
 
     meta = {
         'db_alias': 'core',
