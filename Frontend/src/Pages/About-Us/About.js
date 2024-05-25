3b6601d06cb4b76727d51dd74ce9be5ff785c07a
jen67 <amakrigift2000@gmail.com>
2024-05-25 02:29:13 +0100

    added the mission and vision section to the about us page

--- Frontend/src/Pages/About-Us/About.js
+++ Frontend/src/Pages/About-Us/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "./About.css";
 import Hero from "./HeroSection/Hero";
 import "./HeroSection/Hero.css"; 
+import VisionMission from "./VisMis/VisionMission";
 
 const About = () => {
     return (
         <>
             <Hero />
+            <VisionMission />
         </>
     );
 }
--- Frontend/src/Pages/About-Us/VisMis/VisionMission.js
+++ Frontend/src/Pages/About-Us/VisMis/VisionMission.js
@@ -1,25 +1,36 @@
 // src/components/VisionMission.js
 import React from "react";
 import "./VisionMission.css";
+import images from "../../../Components/images";
+import Footer from "../../../Components/Footer/Footer";
 
 const VisionMission = () => {
   return (
+    <>
     <section className="vision-mission">
       <div className="vision">
+        <img src={images.vision} alt="Vision icon" />
         <h2>Our Vision</h2>
         <p>
           At Wefind, we envision a world where talent meets opportunity
-          seamlessly...
+          seamlessly. Our mission is to create a global ecosystem where
+          individuals find the right opportunities and companies discover their
+          perfect match effortlessly. 
         </p>
       </div>
       <div className="mission">
+        <img src={images.mission} alt="Mission icon" />
         <h2>Our Mission</h2>
         <p>
           At Wefind, our mission is clear: to redefine the way talents and
-          companies connect...
+          companies connect. We're not just a platform; we're a catalyst for
+          change, empowering individuals to unlock their full potential and
+          businesses to thrive in a competitive landscape.
         </p>
       </div>
     </section>
+    <Footer />
+    </>
   );
 };
 
