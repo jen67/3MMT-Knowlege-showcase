0f91c40bf54834d5faa1d8f603f1e2bdb71b3485
jen67 <amakrigift2000@gmail.com>
2024-05-26 23:20:27 +0100

    added more codes to make the dashboard responsive

--- Frontend/src/Pages/Dashboard/withSidebar.js
+++ Frontend/src/Pages/Dashboard/withSidebar.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CompanySidebar from "./CompanyDashboard/Sidebar/CompanySidebar.js"; 
 import TalentSidebar from './TalentDashboard/Sidebar/TalentSidebar';
 import "./withSidebar.css"
 
 const withCompanySidebar = (Component) => {
   return (props) => {
-    const [isSidebarVisible, setSidebarVisible] = useState(true);
+    const [isSidebarVisible, setSidebarVisible] = useState(window.innerWidth > 768);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setSidebarVisible(window.innerWidth > 768);
+      };
+
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     return (
       <div className="dashboard">
         {isSidebarVisible && <CompanySidebar className={isSidebarVisible ? 'visible' : ''} />}
@@ -22,7 +34,19 @@ const withCompanySidebar = (Component) => {
 
 const withTalentSidebar = (Component) => {
   return (props) => {
-    const [isSidebarVisible, setSidebarVisible] = useState(true);
+    const [isSidebarVisible, setSidebarVisible] = useState(window.innerWidth > 768);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setSidebarVisible(window.innerWidth > 768);
+      };
+
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     return (
       <div className="dashboard">
         {isSidebarVisible && <TalentSidebar className={isSidebarVisible ? 'visible' : ''} />}
